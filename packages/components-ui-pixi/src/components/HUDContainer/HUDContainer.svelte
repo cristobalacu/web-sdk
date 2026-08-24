e0d34bf07195c154185a1ef886fa1d8b29689bde
Cristobal Acuña <cristobalacuna5@gmail.com>
2026-08-24 09:04:38 -0400

    feat(components-ui-pixi): agregar HUDContainer (Core UI)

---
+++ packages/components-ui-pixi/src/components/HUDContainer/HUDContainer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { Container, Graphics } from 'pixi-svelte';
+
+	import { getCoreTheme } from '../../theme/context';
+	import { drawDarkGlass } from '../../materials/darkGlass';
+
+	type Props = {
+		width: number;
+		height: number;
+		children: Snippet;
+	};
+
+	const props: Props = $props();
+	const theme = getCoreTheme();
+
+	const draw = (g: import('pixi.js').Graphics) =>
+		drawDarkGlass(theme, g, props.width, props.height, theme.geometry.radius.hud);
+</script>
+
+<Container>
+	<Graphics draw={draw} />
+	{@render props.children()}
+</Container>
